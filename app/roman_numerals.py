75f0c6bc609a520f7a1e775cb9ad07b0f72390e7
Rich-Supe <rsupe1993@aol.com>
2021-07-06 17:21:18 -0400

    part2

--- app/roman_numerals.py
+++ app/roman_numerals.py
@@ -1,2 +1,8 @@
-def parse(num):
-    return 1
+def parse(val):
+    if val == "I":
+        return 1
+    elif val == "II":
+        return 2
+    elif val == "III":
+        return 3
+
--- test/test_roman_numerals_unittest.py
+++ test/test_roman_numerals_unittest.py
@@ -4,5 +4,8 @@ from app.roman_numerals import parse
 class TestRomanNumerals(unittest.TestCase):
     def test_i(self):
         value = parse("I")
-
         self.assertEqual(value, 1)
+
+    def test_ii(self):
+        value = parse("II")
+        self.assertEqual(value, 2)
