e46a1be8fd4dc02f17e6c0f77d2e068f4f997206
Rich-Supe <rsupe1993@aol.com>
2021-07-06 17:35:29 -0400

    finished

--- app/roman_numerals.py
+++ app/roman_numerals.py
@@ -1,8 +1,28 @@
 def parse(val):
-    if val == "I":
-        return 1
-    elif val == "II":
-        return 2
-    elif val == "III":
-        return 3
+    for i in val:
+        total = 0
+        if (i == "I" and (val[i + 1] == "V" or val[i + 1] == "X")):
+            total -= 1
+        if (i == "I"):
+            total += 1
+        if (i == "V"):
+            total += 5
+        if (i == "X"):
+            total += 10
+    # if val == "I":
+    #     return 1
+    # elif val == "II":
+    #     return 2
+    # elif val == "III":
+    #     return 3
+    # elif val == "IV":
+    #     return 4
+    # elif val == "V":
+    #     return 5
+    # elif val == "VI":
+    #     return 6
+    # elif val == "VII":
+    #     return 7
+    # elif val == "VIII":
+    #     return 8
 
--- test/test_roman_numerals_unittest.py
+++ test/test_roman_numerals_unittest.py
@@ -9,3 +9,27 @@ class TestRomanNumerals(unittest.TestCase):
     def test_ii(self):
         value = parse("II")
         self.assertEqual(value, 2)
+    
+    def test_iii(self):
+        value = parse("III")
+        self.assertEqual(value, 3)
+    
+    def test_iv(self):
+        value = parse("IV")
+        self.assertEqual(value, 4)
+    
+    def test_v(self):
+        value = parse("V")
+        self.assertEqual(value, 5)
+
+    # def test_ii(self):
+    #     value = parse("VI")
+    #     self.assertEqual(value, 6)
+
+    # def test_ii(self):
+    #     value = parse("VII")
+    #     self.assertEqual(value, 7)
+    
+    # def test_ii(self):
+    #     value = parse("VIII")
+    #     self.assertEqual(value, 8)
